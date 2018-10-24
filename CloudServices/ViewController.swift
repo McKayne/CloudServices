f0802597cd0a9c32ff663032516d066a5ce670f8
McKayne <mc.kayne@yahoo.com>
2018-10-24 18:44:14 +0300

    Initial Commit

---
+++ CloudServices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudServices
+//
+//  Created by для интернета on 24.10.18.
+//  Copyright © 2018 Nikolay Taran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
